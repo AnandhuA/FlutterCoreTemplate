f2be8f632ec5e47ee2ef3c2b44f655bf701c9d96
Anandhu <anandhu1407@gmail.com>
2026-05-08 12:45:40 +0530

    add lib folder

--- lib/core/themes/app_theme.dart
+++ lib/core/themes/app_theme.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_starater/core/themes/app_colors.dart';
+import 'package:flutter_starater/lib/core/themes/app_colors.dart';
 
 class AppTheme {
   //-------------------Light Theme--------------------------
--- lib/core/themes/theme_extensions.dart
+++ lib/core/themes/theme_extensions.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_starater/core/themes/app_colors.dart';
+import 'package:flutter_starater/lib/core/themes/app_colors.dart';
 
 extension ThemeColors on BuildContext {
   bool get isDarkMode => Theme.of(this).brightness == Brightness.dark;
